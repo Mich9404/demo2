07036709935dda408c1571f59746a7edb25dae5f
Mich9404 <93168275+Mich9404@users.noreply.github.com>
2024-04-26 13:55:19 -0600

    Add files via upload

---
+++ demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -0,0 +1,32 @@
+package com.example.demo2;
+
+import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Demo2Application implements CommandLineRunner {
+
+	private static Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2Application.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		log.info("Hola mundo!!!");
+	}
+
+	@RestController
+	public class MiControlador {
+		@GetMapping("/saludo")
+		public String saludo() {
+			return "¡Hola mundo!";
+		}
+	}
+}
